c4b4b4c89145b0c03f024792a3cf2ca0814ff64f
Chris Fuller <fuller.c.r.89@gmail.com>
2017-10-08 14:10:56 -0400

    Style Changes

--- front-page.php
+++ front-page.php
@@ -1,12 +1,30 @@
 <?php get_header(); ?>
+
+
+
 <div id="intro">
+
+	<div id="container">
+
+		<div class="itemContainer" style="margin-right: 30%;">
+			<img class="alignleft image fit wp-image-51" src="http://www.co2ok.eco/wp-content/uploads/2017/10/CO2ok-knop.png" alt="" width="200" height="120" />
+			<span style="font-weight: 400;color:black;">Verminder je klimaatimpact door de checkbox aan te vinken terwijl je shopt bij onze partners.</span>
+		</div>
+
+		<div class="itemContainer">
+			<img class="alignright image fit wp-image-50" style="margin-left: 15%;" src="http://www.co2ok.eco/wp-content/uploads/2017/10/zon-en-wind-300x180.png" alt="" width="200" height="120" />
+			<span style="font-weight: 400;color:black;">Met uw compensatie worden duurzame energie en CO₂-uitstoot verminderende projecten gefinancierd.</span>
+		</div>
+
+	</div>
 </div>
 
 <div id="main">
+
 	<article class="post featured">
 		<?php while ( have_posts() ) : the_post();
 			the_content();
-			endwhile;
+		endwhile;
 		?>
 		<!-- <ul class="actions">
 			<li><a href="#" class="button big">Full Story</a></li>
--- header.php
+++ header.php
@@ -5,18 +5,72 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
-		
-		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
-		
-		<?php wp_head(); ?>
-	</head>
-	<body class="<?php body_class(); ?> is-loading">
-		<div id="wrapper" class="fade-in">
-			<div class="headlinks" id="nav">
-				<?php wp_nav_menu();?>
-			</div>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body class="<?php body_class(); ?> is-loading">
+
+<div id="wrapper" class="fade-in">
+
+	<style>
+		#wrapper > .bg
+		{
+			background-position:top!important;
+			background-size: contain;
+			top: 115px;
+		}
+
+		#wrapper{
+			background-color: white!important;
+		}
+
+		#main
+		{
+			background-color: transparent!important;
+		}
+		#main>.post {
+			background-color: white!important;
+			padding: 0px 4rem 0px 4rem!important;
+			margin-bottom: 120px;
+		}
+
+		#container
+		{
+
+		}
+		.itemContainer
+		{
+			width: 34%;
+			display: inline-block;
+			align-items: center;
+			background-color: white;
+			border-radius: 15px;
+			border: 1px solid green;
+		}
+
+		.itemContainer p
+		{
+			padding:5px;
+		}
+
+		.itemContainer img
+		{
+			max-width: 220px;
+			margin-left: auto!important;
+			margin-right: auto!important;
+		}
+
+	</style>
+
+
+	<div class="headlinks" id="nav">
+		<?php wp_nav_menu();?>
+	</div>
